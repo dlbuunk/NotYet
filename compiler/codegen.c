e79a11971125bc412c1474f18ffc5fdedeac355f
D.L. Buunk <dlbuunk@gmail.com>
2011-06-26 14:12:11 +0200

    wrote codegen(), started working on comp_block()

--- compiler/codegen.c
+++ compiler/codegen.c
@@ -11,8 +11,40 @@ extern void cerror(char *);
 /* generates intermediate code which than
 * can be send to emit(), which will output asm
 */
+
+void comp_block(unsigned int *b)
+{
+	int i;
+
+	/* some error-checking */
+	if (! (*b & 0x01))
+		cerror("comp_block() attempted to compile unused block");
+	if (*b & 0x04)
+		cerror("comp_block() attemptet to compile data array");
+
+	/* compile all sub-blocks */
+	for (i=3; i<*(b+1);)
+	{
+		if (*(b+i++) == 8)
+			comp_block((unsigned int *) *(b+i++));
+		else i++;
+	}
+}
+
 void codegen(void)
 {
+	int i;
+	int n = 0;
+
+	/* loop through all functions */
+	while (func[++n].u)
+	{
+		if (! func[n].d)
+			continue;
+		if (! func[n].f)
+			cerror("codegen() attempted to compile afunc");
+		comp_block(func[n].b);
+	}
 }
 
 /*********** emitter, outputs asm ************/
@@ -53,7 +85,7 @@ void emit(char *fn)
 						for (i=0; i<var[n].num; i++)
 							fprintf(out, "\t.long\t0x%X\n", *(((unsigned int *) var[n].data.p) + 3 + i));
 					else
-						cerror("codegen() invalid type/size");
+						cerror("emit() invalid type/size");
 				}
 				else /* ! var[n].p */
 				{
@@ -64,7 +96,7 @@ void emit(char *fn)
 					else if (var[n].s == dword)
 						fprintf(out, "\t.long\t0x%X\n", var[n].data.i);
 					else
-						cerror("codegen() invalid type/size");
+						cerror("emit() invalid type/size");
 				}
 			}
 			else /* ! var[n].i */
